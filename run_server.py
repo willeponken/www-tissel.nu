43c239e3b3ede282f10491dba65ccc308b9c78a2
William Wennerström <william@willeponken.me>
2015-10-11 20:46:28 +0200

    Fixed some stuff with the server, added calendar, kinda bad written right now tho

--- run_server.py
+++ run_server.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python2
 import SimpleHTTPServer
 import SocketServer
 
-PORT = 8000
+PORT = 8323
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "serving at port", PORT
+print("serving at port", PORT)
 httpd.serve_forever()
